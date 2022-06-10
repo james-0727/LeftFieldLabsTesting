9b8e79bb8443c787908a4dba60b0f1079ece6ab2
James Wang <james@medabotsworld.com>
2022-06-10 10:26:28 +0800

    added VFX effects

--- Assets/Scripts/Gameplay/Cannonball.cs
+++ Assets/Scripts/Gameplay/Cannonball.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Cannonball : MonoBehaviour
 {
+    public ParticleSystem hitGroundEffect;
+    public ParticleSystem hitEnemyEffect;
     // Start is called before the first frame update
     void Start()
     {
         // destroy 5 seconds after launching
         Destroy(gameObject, 5);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.GetComponent<Enemy>())
+        {
+            hitEnemyEffect.gameObject.SetActive(true);
+        }
+        else
+        {
+            hitGroundEffect.gameObject.SetActive(true);
+        }
     }
 }
